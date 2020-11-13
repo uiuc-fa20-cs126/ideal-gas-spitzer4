b0818a3d89d00342d5f8e617f44d88653347cd49
kaleighspitzer <spitzer4@illinois.edu>
2020-11-13 10:54:57 -0600

    Created a particle class

---
+++ include/particle.h
@@ -0,0 +1,21 @@
+//
+// Created by Kaleigh Spitzer on 11/13/20.
+//
+
+#ifndef IDEAL_GAS_SPITZER4_PARTICLE_H
+#define IDEAL_GAS_SPITZER4_PARTICLE_H
+
+#include "cinder/gl/gl.h"
+#include "cinder/Vector.h"
+
+class Particle {
+    ci::Vec2f position;
+    float radius;
+    float direction;
+    float velocity;
+
+    void Particle::update();
+    void Particle::draw();
+};
+
+#endif //IDEAL_GAS_SPITZER4_PARTICLE_H
--- src/visualizer/sketchpad.cc
+++ src/visualizer/sketchpad.cc
@@ -5,66 +5,12 @@
 #include <visualizer/sketchpad.h>
 
 namespace idealgas {
-
     namespace visualizer {
+        float x = cos(getElapsedSeconds() + getWindowWidth() / 2);
+        float y = sin(getElapsedSeconds() + getWindowHeight() / 2);
+        gl::drawSolidCircle(Vec2f(x, y), 50.0f);
 
-        using glm::vec2;
 
-        idealgas::visualizer::Sketchpad::Sketchpad(const vec2& top_left_corner, size_t num_pixels_per_side,
-                                                     double sketchpad_size, double brush_radius)
-                : top_left_corner_(top_left_corner),
-                  num_pixels_per_side_(num_pixels_per_side),
-                  pixel_side_length_(sketchpad_size / num_pixels_per_side),
-                  brush_radius_(brush_radius) {}
-
-        void idealgas::visualizer::Sketchpad::Draw() const {
-            for (size_t row = 0; row < num_pixels_per_side_; ++row) {
-                for (size_t col = 0; col < num_pixels_per_side_; ++col) {
-                    // Currently, this will draw a quarter circle centered at the top-left
-                    // corner with a radius of 20
-
-                    // TODO: Replace the if-statement below with an if-statement that checks
-                    // if the pixel at (row, col) is currently shaded
-                    if (row * row + col * col <= 20 * 20) {
-                        cinder::gl::color(ci::Color::gray(0.3f));
-                    } else {
-                        cinder::gl::color(ci::Color("white"));
-                    }
-
-                    vec2 pixel_top_left = top_left_corner_ + vec2(col * pixel_side_length_,
-                                                                  row * pixel_side_length_);
-
-                    vec2 pixel_bottom_right =
-                            pixel_top_left + vec2(pixel_side_length_, pixel_side_length_);
-                    cinder::Rectf pixel_bounding_box(pixel_top_left, pixel_bottom_right);
-
-                    cinder::gl::drawSolidRect(pixel_bounding_box);
-
-                    cinder::gl::color(ci::Color("black"));
-                    cinder::gl::drawStrokedRect(pixel_bounding_box);
-                }
-            }
-        }
-
-        void idealgas::visualizer::Sketchpad::HandleBrush(const vec2& brush_screen_coords) {
-            vec2 brush_sketchpad_coords =
-                    (brush_screen_coords - top_left_corner_) / (float)pixel_side_length_;
-
-            for (size_t row = 0; row < num_pixels_per_side_; ++row) {
-                for (size_t col = 0; col < num_pixels_per_side_; ++col) {
-                    vec2 pixel_center = {col + 0.5, row + 0.5};
-
-                    if (glm::distance(brush_sketchpad_coords, pixel_center) <=
-                        brush_radius_) {
-                        // TODO: Add code to shade in the pixel at (row, col)
-                    }
-                }
-            }
-        }
-
-        void idealgas::visualizer::Sketchpad::Clear() {
-            // TODO: implement this method
-        }
 
     }  // namespace visualizer
 
