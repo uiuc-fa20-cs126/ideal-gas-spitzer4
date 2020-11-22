53387de8417215945a031ec03bbc10959fa5b67e
kaleighspitzer <spitzer4@illinois.edu>
2020-11-22 10:53:02 -0600

    Added boundary

--- src/visualizer/IdealGasApp.cc
+++ src/visualizer/IdealGasApp.cc
@@ -4,21 +4,29 @@
 
 #include <numeric>
 #include "/Users/kaleighspitzer/CLionProjects/~Cinder/my-projects/ideal-gas-spitzer4/include/visualizer/IdealGasApp.h"
-#include "/Users/kaleighspitzer/CLionProjects/~Cinder/my-projects/ideal-gas-spitzer4/include/visualizer/simulation.h"
 
 using namespace idealgas;
 
-idealgas::Particle particle1({60, 60});
-idealgas::Particle particle2({100, 100});
+//idealgas::Particle particle1({60, 60});
+//idealgas::Particle particle2({100, 100});
+idealgas::Particle particle1({rand() % 601, rand() % 601});
+idealgas::Particle particle2({rand() % 601, rand() % 601});
+
+size_t minWidth = 115;
+size_t maxWidth = 685;
+size_t minHeight = 115;
+size_t maxHeight = 685;
 
 IdealGasApp::IdealGasApp() {
-    setWindowSize(600, 600);
+    setWindowSize(800, 800);
 }
 
 void IdealGasApp::setup() {
-    particle1.position = {60, 60};
+//    particle1.position = {60, 60};
+    particle1.position = {rand() % 601, rand() % 601};
     particle1.velocity = {2.0, 2.0};
-    particle2.position = {100, 100};
+//    particle2.position = {100, 100};
+    particle2.position = {rand() % 601, rand() % 601};
     particle2.velocity = {2.0, 3.0};
 }
 
@@ -30,10 +38,10 @@ void IdealGasApp::changeVelocity(idealgas::Particle& particle1, idealgas::Partic
          float velocityYCoord = particle1.velocity.operator[](1);
          float positionXCoord = particle1.position.operator[](0);
          float positionYCoord = particle1.position.operator[](1);
-         if (positionXCoord > getWindowWidth() || positionXCoord < 0) {
+         if (positionXCoord > maxWidth || positionXCoord < minWidth) {
              velocityXCoord = -velocityXCoord;
          }
-         if (positionYCoord > getWindowHeight() || positionYCoord < 0) {
+         if (positionYCoord > maxHeight || positionYCoord < minHeight) {
              velocityYCoord = -velocityYCoord;
          }
          particle1.velocity = {velocityXCoord, velocityYCoord};
@@ -66,6 +74,11 @@ void IdealGasApp::update() {
 
 void IdealGasApp::draw() {
     ci::gl::clear(ci::Color(0, 0, 0));
+    ci::Rectf boundary(100, 100, 700, 700);
+    ci::gl::color(ci::Color(0, 1, 0));
+    ci::gl::drawStrokedRect(boundary);
+//    ci::gl::clear(ci::Color(0, 0, 0));
+//    ci::gl::color(ci::Color(0, 0, 0));
     particle1.draw();
     particle2.draw();
 }
