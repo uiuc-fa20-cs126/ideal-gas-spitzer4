5acae98846a5401926d44037fed5a0d0840bfca8
kaleighspitzer <spitzer4@illinois.edu>
2020-12-18 10:19:37 -0600

    Implemented IdealGasApp.h

--- include/visualizer/IdealGasApp.h
+++ include/visualizer/IdealGasApp.h
@@ -5,4 +5,19 @@
 #ifndef IDEAL_GAS_SPITZER4_2_IDEALGASAPP_H
 #define IDEAL_GAS_SPITZER4_2_IDEALGASAPP_H
 
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include ".../core/Particle.h"
+
+class IdealGasApp : public ci::app::App {
+public:
+    IdealGasApp();
+    void setup();
+    void update();
+    void draw();
+    static void changeVelocity(idealgas::Particle&, idealgas::Particle&, bool);
+};
+
 #endif //IDEAL_GAS_SPITZER4_2_IDEALGASAPP_H
