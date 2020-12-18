dfdead5622c1c4ea3ca2199cb3ec2c93d0527308
kaleighspitzer <spitzer4@illinois.edu>
2020-12-18 10:10:26 -0600

    Implemented Particle.h

--- include/core/Particle.h
+++ include/core/Particle.h
@@ -5,4 +5,20 @@
 #ifndef IDEAL_GAS_SPITZER4_2_PARTICLE_H
 #define IDEAL_GAS_SPITZER4_2_PARTICLE_H
 
+namespace idealgas {
+    class Particle {
+    public:
+        glm::vec2 position;
+        glm::vec2 velocity;
+        float radius;
+        float time;
+
+        Particle();
+
+        void update();
+
+        void draw();
+    };
+}
+
 #endif //IDEAL_GAS_SPITZER4_2_PARTICLE_H
